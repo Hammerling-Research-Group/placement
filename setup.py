b45dee2a8bf3eec283357b1e5c1546109b5d373a
Philip Waggoner <31326382+pdwaggoner@users.noreply.github.com>
2024-10-03 11:16:33 -0600

    Create setup.py
    creating metadata via `setup.py`

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='placement',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=['numpy', 'setuptools'],
+    author='Meng Jia, Troy Sorensen, Philip Waggoner, Dorit Hammerling', 
+    author_email='mjia@mines.edu, trsorensen@mines.edu, philip.waggoner@mines.edu, hammerling@mines.edu', 
+    maintainer='Meng Jia',
+    maintainer_email='mjia@mines.edu',
+    description='A Python package for optimizing continuous monitoring sensor placement on oil and gas sites',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Hammerling-Research-Group/placement',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
