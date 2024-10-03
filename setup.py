7578dc39479dcf003f415732d3f8df6ff9c2f32c
Philip Waggoner <31326382+pdwaggoner@users.noreply.github.com>
2024-10-03 11:32:11 -0600

    Update setup.py
    fixes in response to issues noted in the initial lint check

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+"""
+Setup configuration for the placement package.
+"""
+
 from setuptools import setup, find_packages
 
 setup(
@@ -5,12 +9,14 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=['numpy', 'setuptools'],
-    author='Meng Jia, Troy Sorensen, Philip Waggoner, Dorit Hammerling', 
-    author_email='mjia@mines.edu, trsorensen@mines.edu, philip.waggoner@mines.edu, hammerling@mines.edu', 
+    author='Meng Jia, Troy Sorensen, Philip Waggoner, Dorit Hammerling',
+    author_email='mjia@mines.edu, trsorensen@mines.edu, philip.waggoner@mines.edu, hammerling@mines.edu',
     maintainer='Meng Jia',
     maintainer_email='mjia@mines.edu',
     description='A Python package for optimizing continuous monitoring sensor placement on oil and gas sites',
-    long_description=open('README.md').read(),
+    long_description=(
+        open('README.md', encoding='utf-8').read()
+    ),
     long_description_content_type='text/markdown',
     url='https://github.com/Hammerling-Research-Group/placement',
     classifiers=[
