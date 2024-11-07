7e8abc74ad88ebca57c57d567c5b4236f47a3d8c
Philip Waggoner <31326382+pdwaggoner@users.noreply.github.com>
2024-11-07 13:04:31 -0700

    Update test_simulate_concentrations.py

--- tests/test_simulate_concentrations.py
+++ tests/test_simulate_concentrations.py
@@ -5,7 +5,8 @@ from datetime import datetime, timezone
 import os
 import tempfile
 import shutil
-from simulate_concentrations import run_gp
+#from simulate_concentrations import run_gp
+from placement.simulate_concentrations import run_gp
 
 
 class TestSimulateConcentrations(unittest.TestCase):
